87375edc862bf6774007a5acd599dce11c7df192
Markov Vladimir <84292343+I4Rt@users.noreply.github.com>
2023-09-15 19:00:58 +0700

    front-end
     - login/logout for user "admin" with password "password"
     - bootstrap now cover all abilities;

--- FileUtil.py
+++ FileUtil.py
@@ -1,15 +1,32 @@
-import base64
+from __future__ import annotations
+from typing import List
 import cv2
-from numpy import *
+from numpy import frombuffer
+import json
+import base64
 
-def convertImageToBytes(img, imgType = '.png') -> str:
-        _, encriptedImg = cv2.imencode(imgType, img)
+class FileUtil:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def convertImageToBytes(img, imgType = '.png') -> str:
+        _, encriptedImg = 	cv2.imencode(imgType, img)
         imgAsStr = encriptedImg.tostring()
         imgByteStr = base64.b64encode(imgAsStr).decode("utf-8")
         return imgByteStr
     
-def convertBytesToImg(encImg: str):
+    @staticmethod
+    def convertBytesToImg(encImg: str):
         readImgBytes = base64.b64decode(encImg)
-        npImg = frombuffer(readImgBytes, uint8) 
-        decImg = cv2.imdecode(npImg, -1)
+        npImg = frombuffer(readImgBytes,'u1') 
+        decImg = cv2.imdecode(npImg, 1)
         return decImg
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    _, img = cap.read()
+    print(FileUtil.convertImageToBytes(img))
+    
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect, url_for
 from time import time
 from flask_jwt_extended import *
 from flask_cors import CORS, cross_origin
@@ -6,9 +6,18 @@ from FileUtil import *
 from datetime import datetime
 import json
 import os
+import flask_login
+import shutil
+
 directory = os.getcwd()
 
 app = Flask(__name__, template_folder='templates', static_folder='images',static_url_path = '')
+
+# auth
+app.secret_key = 'U wont guess it'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
 
 app.config['TIME'] = time()
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -16,9 +25,60 @@ UPLOAD_FOLDER = directory + '/images'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-
 cors = CORS(app)
+
+users = {'admin': {'password': 'password'}}
+class User(flask_login.UserMixin):
+    pass
+
+def deleteForce(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        print(len(dirs), len(files))
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
+
+
+@login_manager.user_loader
+def user_loader(login):
+    if login not in users:
+        return
+    user = User()
+    user.id = login
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    login = request.form.get('login')
+    if login not in users:
+        return
+    user = User()
+    user.id = login
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html', class_ ='', message='')
+
+    login = request.form['login']
+    if login in users and request.form['password'] == users[login]['password']:
+        user = User()
+        user.id = login
+        flask_login.login_user(user)
+        return redirect(url_for('getMain'))
+    return render_template('login.html', class_ ='text-danger', message='Неверные данные')
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return render_template('login.html', class_ ='text-dark', message='Вы вышли из аккаунта')
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('login.html', class_ ='', message='')
 
 @cross_origin
 @app.route('/setWarning', methods=['get', 'post'])
@@ -29,14 +89,15 @@ def setWarning():
     try:
         
         sender = request.remote_addr
-        sender.replace('.', '_')
+        # sender.replace('.', '_')
+        userName = request.json['clientName']
         encImg = request.json['img']
     except Exception:
         print({'Message': 'Json is not valid'})
         return make_response({'Message': 'Json is not valid'})
     
     try:
-        image = convertBytesToImg(encImg)
+        image = FileUtil.convertBytesToImg(encImg)
         if image is None:
             return make_response({'Message': 'Image decoding error'})
     except:
@@ -47,8 +108,10 @@ def setWarning():
     path = os.path.join(path, sender) 
     if not os.path.exists(path):
         os.mkdir(path)
-    print(path)
-    print(sender)
+        with open(directory + f'/images/{path}/data.txt', 'w') as file:
+            file.write(json.dumps({'userName': userName}))
+    # print(path)
+    # print(sender)
     try:
         result = cv2.imwrite(f'images/{sender}/{curTime}.jpg', image)
         if not result:
@@ -62,6 +125,7 @@ def setWarning():
 
 # page
 @app.route('/main')
+@flask_login.login_required
 def getMain():
     try:
         # request.json['param']
@@ -73,14 +137,26 @@ def getMain():
     except:
         selectedWarn = None
     print(selectedWarn)
-        
+    
+    userList = []
+    for path in os.listdir(os.path.join(directory, 'images')):
+        try:
+            if path != "sys":
+                with open(directory + f'/images/{path}/data.txt', 'r') as file:
+                    data = json.loads(file.read())
+                user = [path, data['userName']] 
+                userList.append(user)
+        except Exception as e:
+            print(e)
+            
     warns = []
     imgSrc = ""
     if selected != None:
-        warns = [path for path in os.listdir(os.path.join(directory, f'images/{selected}'))]
+        warns = [[path, f'{path[0:2]}/{path[3:5]}/{path[6:10]} {path[13:15]}:{path[16:18]}:{path[19:21]}'] for path in os.listdir(os.path.join(directory, f'images/{selected}/images'))]
         if selectedWarn != None:
-            imgSrc = f'{selected}/{selectedWarn}'
-    return render_template('main.html', users = [path for path in os.listdir(os.path.join(directory, 'images')) if path != "sys"], warnings = warns, curUser=selected,img=imgSrc)
+            imgSrc = f'{selected}/images/{selectedWarn}'
+            
+    return render_template('main.html', users = userList, warnings = warns, curUser=selected,img=imgSrc)
 
 @app.route('/del')
 def delete():
@@ -89,11 +165,14 @@ def delete():
         imgPath = request.args.get('imgPath')
         print(f'removing: {directory + "/images/" +imgPath}')
         os.remove(directory + '/images/' +imgPath)
-        print('len is', len(os.listdir(os.path.join(directory, 'images/' + imgPath[:-25]))))
+        print('len is', len(os.listdir(os.path.join(directory, 'images/' + imgPath[:-25]))), directory + '/images/' + imgPath[:-25])
         if len(os.listdir(os.path.join(directory, 'images/' + imgPath[:-25]))) == 0:
-            os.rmdir(os.path.join(directory, 'images/' + imgPath[:-25]))
+            print(directory + '/images/' + imgPath[:-32])
+            shutil.rmtree(directory + '/images/' + imgPath[:-32])
+            return redirect(url_for('getMain'))
         return getMain()
-    except:
+    except Exception as e:
+        print(e)
         return """
                 <html>
                     <head>
